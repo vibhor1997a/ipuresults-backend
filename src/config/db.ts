19ae3553f71f2a99298edf9be0d4ef906d6a8484
vibhor agrawal <vibhor349a@rediffmail.com>
2019-09-15 14:42:11 +0530

    change db

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,12 +8,12 @@ import { subjectSchema } from '../schemas/subject';
 
 export async function connectToDB(conn: mongoose.Connection) {
     if (!conn) {
-        const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/host4free?retryWrites=true&w=majority`;
+        const dbURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
         console.log('Connecting to Db!');
         conn = await mongoose.createConnection(dbURI, {
             bufferCommands: false,
             bufferMaxEntries: 0,
-            useNewUrlParser: true
+            useNewUrlParser: true,
         });
         console.log('Connected to Db!');
         conn.model('ResultFile', resultFileSchema);
